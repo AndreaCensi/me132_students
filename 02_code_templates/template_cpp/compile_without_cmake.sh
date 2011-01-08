fbb1ad611b9c7cb0be70079c0f45f8732ab646a8
Andrea Censi <andrea@cds.caltech.edu>
2011-01-07 21:35:32 -0800

    Updated paths

--- 02_code_templates/template_cpp/compile_without_cmake.sh
+++ 02_code_templates/template_cpp/compile_without_cmake.sh
@@ -3,9 +3,9 @@ set -e # stop on errors (always include this in your scripts)
 set -x # echo commands 
 
 
-sources="my_functions.cc cmdline_parsing.cc basic_client.cc"
+sources="src/my_functions.cc src/cmdline_parsing.cc src/basic_client.cc"
 
 # Note this assumes that PKG_CONFIG_PATH is setup correctly
 libraries=`pkg-config --cflags --libs playerc++`
 
-g++  ${libraries} ${sources} -o my_client
+g++  ${libraries} ${sources} -o my_cpp_client
